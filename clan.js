524f0045e88170d6ab9476001d0218dac9cf48c0
bfallin <fallinbryan@gmail.com>
2018-07-20 12:48:57 -0500

    Move player stats to live API call

--- clan.js
+++ clan.js
@@ -23,6 +23,7 @@ $(document).ready(function () {
 			$("#hero-table").slideUp();
 			$("#achievement-table").slideUp();
 		 });
+
 		
 		populuateClanPage(defaultClanTag)
 	  
@@ -30,99 +31,105 @@ $(document).ready(function () {
 });
 
 //// end of doc ready
-function buildModalFromPlayer(playerName) {
-	var player = getPlayer(playerName);
-	var playerTownHall = player['townHallLevel'];
-	var troops = player['troops'];
-	var spells = player['spells'];
-	var heros = player['heroes'];
-	var achievements = player['achievements'];
-	var role = player['role'];
-	if(role == 'admin') {
-		role = 'elder'
-	}
-	
-	$('#modal-player-name').html(player['name']);
-	$('#table-player-name').html(player['name']);
-	$('#modal-player-role').html(role);
-	$('#playerLevel').html(player['expLevel']);
-	$('#townHallLevel').html(player['townHallLevel']);
-	$('#builderHallLevel').html(player['builderHallLevel']);
-	$('#playerTrophies').html(player['trophies']);
-	$('#playerBestTrophies').html(player['bestTrophies']);
-	$('#playerWarStars').html(player['warStars']);
-	$('#playerAttackWins').html(player['attackWins']);
-	$('#playerDefenseWins').html(player['defenseWins']);
-	
-	$('.modal-title').text('Player Data Sheet');
-	try {
-		$('#modal-player-badge').attr("src",player['league']['iconUrls']['medium']);
-	}catch(err) {
-		$('#modal-player-badge').attr("src",'');
-	}
-	
-	
-	$('#troop-data').empty();
-	for(var i = 0; i < troops.length; i++) {
-		var row = $('<tr>');
-		row.append( $('<td>').text(troops[i]['name'] ));
-		row.append( $('<td>').html('<b>' + troops[i]['level'] + '</b>/'+ troops[i]['maxLevel'] ));
-		row.append( $('<td>').text(troops[i]['village'] ));
-		$('#troop-data').append(row);
-	}
-	$('#spell-data').empty();
-	for(var i = 0; i < spells.length; i++) {
-		var row = $('<tr>');
-		var row = $('<tr>');
-		row.append( $('<td>').text(spells[i]['name'] ));
-		row.append( $('<td>').html('<b>' + spells[i]['level'] + '</b>/'+ spells[i]['maxLevel'] ));
-		row.append( $('<td>').text(spells[i]['village'] ));
-		$('#spell-data').append(row);
-	}
-	
-	$('#hero-data').empty();
-	for(var i = 0; i < heros.length; i++) {
-		var row = $('<tr>');
-		var row = $('<tr>');
-		row.append( $('<td>').text(heros[i]['name'] ));
-		row.append( $('<td>').html('<b>' + heros[i]['level'] + '</b>/'+ heros[i]['maxLevel'] ));
-		row.append( $('<td>').text(heros[i]['village'] ));
-		$('#hero-data').append(row);
-	}
-	
-	$('#achievement-data').empty();
-	
-	for(var i = 0; i < achievements.length; i++) {
-		var row = $('<tr>');
-		var row = $('<tr>');
-		row.append( $('<td>').text(achievements[i]['name'] ));
-		row.append( $('<td>').text(achievements[i]['stars'] ));
-		row.append( $('<td>').text(achievements[i]['value'] + '/'+ achievements[i]['target'] ));
-		row.append( $('<td>').text(achievements[i]['info'] ));
-		try {
-			row.append( $('<td>').text(achievements[i]['completionInfo'] ));
-		} catch {
-			console.log(err);
-			row.append( $('<td>').text('N/A'));
-		}
-		row.append( $('<td>').text(achievements[i]['village'] ));
-		if(achievements[i]['stars'] == 3) {
-			row.addClass('success');
-		} else if (achievements[i]['stars'] == 2) {
-			row.addClass('warning');
-		} else if (achievements[i]['stars'] == 1) {
-			row.addClass('info');
-		} else if (achievements[i]['stars'] == 0) {
-			row.addClass('danger');
-		}
-		
-		
-		$('#achievement-data').append(row);
-	}
-	
-	$("#playerData").modal();
-}
 
+
+function buildModalFromPlayer(playerTag) {
+	var uri='player?tag=' + playerTag.replace('#','%23');
+	$.getJSON( uri, function(data, staus, result) {
+		var player = data;
+		var playerTownHall = player['townHallLevel'];
+		var troops = player['troops'];
+		var spells = player['spells'];
+		var heros = player['heroes'];
+		var achievements = player['achievements'];
+		var role = player['role'];
+		if(role == 'admin') {
+			role = 'elder'
+		}
+		
+		$('#modal-player-name').html(player['name']);
+		$('#table-player-name').html(player['name']);
+		$('#modal-player-role').html(role);
+		$('#playerLevel').html(player['expLevel']);
+		$('#townHallLevel').html(player['townHallLevel']);
+		$('#builderHallLevel').html(player['builderHallLevel']);
+		$('#playerTrophies').html(player['trophies']);
+		$('#playerBestTrophies').html(player['bestTrophies']);
+		$('#playerWarStars').html(player['warStars']);
+		$('#playerAttackWins').html(player['attackWins']);
+		$('#playerDefenseWins').html(player['defenseWins']);
+		
+		$('.modal-title').text('Player Data Sheet');
+		try {
+			$('#modal-player-badge').attr("src",player['league']['iconUrls']['medium']);
+		}catch(err) {
+			$('#modal-player-badge').attr("src",'');
+		}
+		
+		
+		$('#troop-data').empty();
+		for(var i = 0; i < troops.length; i++) {
+			var row = $('<tr>');
+			row.append( $('<td>').text(troops[i]['name'] ));
+			row.append( $('<td>').html('<b>' + troops[i]['level'] + '</b>/'+ troops[i]['maxLevel'] ));
+			row.append( $('<td>').text(troops[i]['village'] ));
+			$('#troop-data').append(row);
+		}
+		$('#spell-data').empty();
+		for(var i = 0; i < spells.length; i++) {
+			var row = $('<tr>');
+			var row = $('<tr>');
+			row.append( $('<td>').text(spells[i]['name'] ));
+			row.append( $('<td>').html('<b>' + spells[i]['level'] + '</b>/'+ spells[i]['maxLevel'] ));
+			row.append( $('<td>').text(spells[i]['village'] ));
+			$('#spell-data').append(row);
+		}
+		
+		$('#hero-data').empty();
+		for(var i = 0; i < heros.length; i++) {
+			var row = $('<tr>');
+			var row = $('<tr>');
+			row.append( $('<td>').text(heros[i]['name'] ));
+			row.append( $('<td>').html('<b>' + heros[i]['level'] + '</b>/'+ heros[i]['maxLevel'] ));
+			row.append( $('<td>').text(heros[i]['village'] ));
+			$('#hero-data').append(row);
+		}
+		
+		$('#achievement-data').empty();
+		
+		for(var i = 0; i < achievements.length; i++) {
+			var row = $('<tr>');
+			var row = $('<tr>');
+			row.append( $('<td>').text(achievements[i]['name'] ));
+			row.append( $('<td>').text(achievements[i]['stars'] ));
+			row.append( $('<td>').text(achievements[i]['value'] + '/'+ achievements[i]['target'] ));
+			row.append( $('<td>').text(achievements[i]['info'] ));
+			try {
+				row.append( $('<td>').text(achievements[i]['completionInfo'] ));
+			} catch {
+				console.log(err);
+				row.append( $('<td>').text('N/A'));
+			}
+			row.append( $('<td>').text(achievements[i]['village'] ));
+			if(achievements[i]['stars'] == 3) {
+				row.addClass('success');
+			} else if (achievements[i]['stars'] == 2) {
+				row.addClass('warning');
+			} else if (achievements[i]['stars'] == 1) {
+				row.addClass('info');
+			} else if (achievements[i]['stars'] == 0) {
+				row.addClass('danger');
+			}
+			
+			
+			$('#achievement-data').append(row);
+		}
+		
+		$("#playerData").modal();
+	
+	});
+}
+//end of modal builder
 function getPlayer(playerName) {
 	var i;
 	var len = memberData['memberList'].length;
@@ -174,12 +181,10 @@ function populuateClanPage(clanTag) {
 					var image = document.createElement('img');
 					image.setAttribute("src",data["memberList"][i]["league"]['iconUrls']['small'])
 					tableData.prepend(image);
-					$(tableData).click(function() {
-						buildModalFromPlayer($(this).text())
-						
-						
-					});
 					row.append(tableData);
+				} else if (tag == "tag") {
+					
+					row.append($('<td id="playerTag" style="vertical-align:middle"></td>').text(tag));
 				}else if (tag == "donations" ) {
 						donations = data["memberList"][i]["donations"];
 				} else if (tag == "donationsReceived") {
@@ -202,6 +207,9 @@ function populuateClanPage(clanTag) {
 			if(donationRation < 0.5) {
 				row.addClass("warning");
 			}
+			$(row).click(function() {
+						buildModalFromPlayer($(this).find('#playerTag').text())
+					});
 			tableBody.append(row);
 		}
 		playerTable.append(tableBody);
